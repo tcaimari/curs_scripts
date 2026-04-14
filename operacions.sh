30f99ddc88efdcb54c497d053984c9bfe14ae2a0
Toni <tcaimaricr@esliceu.net>
2026-04-14 16:46:16 +0200

    README.md

---
+++ operacions.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#variables son "paraules" que contenen un valor
+NOM="toni"
+#read seveix per llegir informacio del teclat
+read -p "introdueix un nom: " NOM
+
+echo "El meu nom és ${NOM}"
