1e1b05cc31bc08d6f2453371f61776f20dbf40d0
Toni <tcaimaricr@esliceu.net>
2026-04-15 15:50:14 +0200

    script usuaris acabat

--- crear_user.sh
+++ crear_user.sh
@@ -4,7 +4,7 @@ function usage(){
     echo
     echo "has de ser root"
     echo "l'usuari no és pot repetir"
-    exit 1
+    exit 1 #indica que hi ha hagut una errada
 }
 #Script per crear un usuari
 
@@ -20,4 +20,22 @@ read -p "Introdueix el nom d'usuari: " USER_NAME
 read -p "Introdueix el password: " PASSWORD
 
 #creariem l'usuari
-useradd -m -c "${COMMENTS}" ${USER_NAME}
+useradd -m -c "${COMMENTS}" ${USER_NAME} &> /dev/null
+#control de la darrera comanda
+if [[ ${?} -ne 0 ]]
+then
+    echo "Errada creant l'usuari"
+    exit 1
+fi
+
+#canvi de password
+echo "${USER_NAME}:${PASSWORD}" | chpasswd
+#comprovam si el canvi de password ha anat bé.
+if [[ ${?} -ne 0 ]]
+then
+    echo "Errada Canviant password"
+    exit 1
+fi
+
+#fer caducar el password
+passwd -e ${USER_NAME}
